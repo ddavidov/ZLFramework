bcd98c8edb1a936fa8ad7925c2142c2a824e0232
ddavidov <ddavidov@mindk.com>
2014-11-18 10:46:56 +0200

    Server CURL extension config check

--- plg_zlframework/zlframework/helpers/zlfw/check.php
+++ plg_zlframework/zlframework/helpers/zlfw/check.php
@@ -7,12 +7,76 @@
 class zlfwHelperCheck extends AppHelper {
 
 	/**
+	 * @var Messages stack
+	 */
+	protected static $stack;
+
+	/**
+	 * Add message to stack
+	 *
+	 * @param   string
+	 * @param   string
+	 *
+	 * @return  void
+	 */
+	public function addMsg($message_text, $namespace = 'error'){
+
+		if(array_key_exists($namespace, self::$stack)){
+			self::$stack[$namespace][] = JText::_($message_text);
+		}else{
+			self::$stack[$namespace] = array(JText::_($message_text));
+		}
+	}
+
+	/**
+	 * Get messages
+	 *
+	 * @param   string
+	 *
+	 * @return  array
+	 */
+	public function getMsg($namespace = NULL){
+		$buffer = array();
+
+		if(!empty($namespace)){
+			if(array_key_exists($namespace, self::$stack)){
+				$buffer = self::$stack[$namespace];
+			}
+		}else{
+			if(!empty(self::$stack)){
+				// Mix and return messages from all namespaces:
+				foreach(self::$stack as $group => $messages){
+					$buffer = array_merge($buffer, $messages);
+				}
+			}
+		}
+
+		return $buffer;
+	}
+
+	/**
 	 * Check curl options
+	 *
+	 * @return  bool
 	 */
 	public function checkCurl(){
-		$errors = array();
 
-		function_exists('curl_version') ? 'Enabled' : 'Disabled';
+		$success = function_exists('curl_version');
+
+		if(!$success){
+			$this->addMsg('PLG_ZLFRAMEWORK_CURL_NOT_INSTALLED');
+		}else{
+			// Check further
+			$version = curl_version();
+			$ssl_support = ($version['features'] & CURL_VERSION_SSL);
+			$success = $success && $ssl_support;
+
+			if(!$ssl_support){
+				$this->addMsg('PLG_ZLFRAMEWORK_CURL_SSL_NOT_SUPPORTED', 'warning');
+			}
+		}
+
+		return $success;
 	}
 }
 
